c919d05d248ac84fbf980b5b8332898c125cec9c
Avdi Grimm & Chris Strom <pair+avdi+chris@pivotallabs.com>
2010-04-14 22:48:03 -0400

    Refactored report.rb for exercise 5.2

--- robot/report.rb
+++ robot/report.rb
@@ -1,18 +1,30 @@
 class Report
   def Report.report(out, machines, robot)
     out.print "FACTORY REPORT\n"
-    machines.each do |machine|
-      out.print "Machine #{machine.name}"
-      out.print " bin=#{machine.bin}" if machine.bin != nil
-      out.print "\n"
-    end
-    out.print "\n"
+    report_machines(out, machines)
+    report_robot(out, robot)
+    out.print "========\n"
+  end
+
+  def self.report_robot(out, robot)
     out.print "Robot"
     if robot.location != nil
       out.print " location=#{robot.location.name}"
     end
-    out.print " bin=#{robot.bin}" if robot.bin != nil
+    report_bin(out, robot)
     out.print "\n"
-    out.print "========\n"
+  end
+
+  def self.report_machines(out, machines)
+    machines.each do |machine|
+      out.print "Machine #{machine.name}"
+      report_bin(out, machine)
+      out.print "\n"
+    end
+    out.print "\n"
+  end
+
+  def self.report_bin(out, device)
+    out.print " bin=#{device.bin}" if device.bin != nil
   end
 end
